84a26a8692a079684ccffe31956ee241ada265a2
nasywzhra <nasywazahranailah@gmail.com>
2026-03-29 21:28:11 +0700

    merge generic-class

--- Program.cs
+++ Program.cs
@@ -8,11 +8,29 @@ class HaloGeneric
     }
 }
 
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
         HaloGeneric halo = new HaloGeneric();
-        halo.SapaUser<string>("Nasywa"); 
+        halo.SapaUser<string>("Nasywa");
+
+        DataGeneric<string> data = new DataGeneric<string>("103022430005");
+        data.PrintData();
     }
 }
